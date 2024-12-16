001b5b14602b9d65ad15f88007577cedb29d260a
joamontesgi <joamontesgi@unal.edu.co>
2024-12-16 18:36:50 -0500

    clase 16 diciembre

--- app/Http/Controllers/SaleController.php
+++ app/Http/Controllers/SaleController.php
@@ -7,16 +7,28 @@ use Illuminate\Support\Facades\Http;
 class SaleController extends Controller
 {
 protected $apiUrl;
+protected $apiKey;
     public function __construct()
     {
         $this->apiUrl = env('MICROSERVICIO_API_URL');
+        $this->apiKey = env('API_KEY');
     }
-    
+
+    // public function index()
+    // {
+    //     $url=$this->apiUrl.'/sales/';
+    //     $response=Http::get($url);
+    //     return $response->json();
+    // }
+
+
     public function index()
     {
-        $url=$this->apiUrl.'/sales/';
-        $response=Http::get($url);
+        $url = $this->apiUrl . '/sales/';
+        $response = Http::withHeaders(['X-API-Key' => $this->apiKey])->get($url);
         return $response->json();
     }
+    
+
 
 }
